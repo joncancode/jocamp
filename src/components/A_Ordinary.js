c0a3eb9b9d80fbb075783b733c07f9803fec47e6
joncancode <jc.haines19@gmail.com>
2017-12-03 22:28:29 -0500

    initial creation of first two components at 130vh

---
+++ src/components/A_Ordinary.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './A_Ordinary.css';
+
+class A_Ordinary extends Component {
+  render() {
+    return (
+      <div className="A_Ordinary">
+        <h1>first </h1>
+
+      </div>
+    );
+  }
+}
+
+export default A_Ordinary;
---
+++ src/components/B_Call.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './B_Call.css';
+
+class B_Call extends Component {
+  render() {
+    return (
+      <div className="B_Call">
+        <h1>second </h1>
+
+      </div>
+    );
+  }
+}
+
+export default B_Call;
