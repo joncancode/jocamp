4c38008d4db1f80ac48b8c5c8c9cbb03ef9299a2
joncancode <jc.haines19@gmail.com>
2017-12-04 00:25:43 -0500

    tweenlite scroll test

--- src/components/B_Call.js
+++ src/components/B_Call.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './B_Call.css';
+import { TweenLite } from 'gsap';
 
 class B_Call extends Component {
+
+    componentDidMount(){
+
+        TweenLite.to(window, 2.8, {scrollTo: '#myId'});
+        
+        
+    }
+
+
   render() {
     return (
       <div className="B_Call">
         <h1>second </h1>
-
+        <div id="myId">test</div>
       </div>
     );
   }
